285457d66230ea5f67a77bb4eb1b7392caadeae4
Joseph Gabrie <josephgabriemolina@gmail.com>
2024-08-16 16:59:53 -0400

    lol

--- JapaneLanguageCompanion/Frontend/public/index.js
+++ JapaneLanguageCompanion/Frontend/public/index.js
@@ -1,16 +1,18 @@
 fetch(" http://127.0.0.1:3000/ ")
     .then(res => res.json())
     .then(data => {
-        const characterData = data.find(item => item.kanakanji_id === 6);
-        if (characterData) {
-            console.log(characterData.character);
-            document.getElementById("japLetter").innerHTML = characterData.character
-        } else {
-            console.log("Character not found");
-        }
-    })
-
-
-    .catch(error => console.error(error));
+        const arrayLength = data.length;
+        
+        for(let i=0; i< arrayLength; i++) {
+            const characterData= data.find(item => item.kanakanji_id === i);
+            if (characterData) {
+                console.log(characterData.character);
+                document.getElementById("japLetter").innerHTML = characterData.character
+            }
+            console.log(data[i]);
 
     
+        }
+        
+    })
+    .catch(error => console.error(error))
