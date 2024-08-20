567b1e509f2d58c9a67d588ba417e61698577d00
joshu <joshuagabrie@gmail.com>
2024-08-20 13:15:10 -0400

    created a website for the flashcard

--- JapaneLanguageCompanion/Frontend/public/index.js
+++ JapaneLanguageCompanion/Frontend/public/index.js
@@ -16,3 +16,37 @@ fetch(" http://127.0.0.1:3000/ ")
         
     })
     .catch(error => console.error(error))
+
+    
+    // Do some reasearch on thisDocument Object Model 
+const term= document.querySelector('.term');
+const checkButton = document.querySelector('.check');
+const definition = document.querySelector('.definition')
+const nextButton = document.querySelector('.next')
+
+words = {
+    Hello: "Hola",
+    Goodbye: "Adios",
+    "I drink water": "Yo tomo agua"
+ }
+
+data = Object.entries(words)
+
+function getRandomWord(){
+    randomTerm=data[Math.floor(Math.random() * data.length)]
+    term.innerHTML = `<h3>${randomTerm[0]}</h3>`;
+    definition.innerHTML = ` <h3>${randomTerm[1]}</h3`
+}
+
+data = Object.entries(words)
+console.log(data[0][0])
+
+checkButton.addEventListener('click',function(){
+    definition.style.display = 'block';
+});
+nextButton.addEventListener('click',function(){
+    getRandomWord();
+});
+
+
+    
